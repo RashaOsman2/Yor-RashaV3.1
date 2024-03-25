22cf70c9c7a137bb2a9f4b05f8fb9a7ae74f8bab
RashaOsman2 <101505051+RashaOsman2@users.noreply.github.com>
2024-03-25 23:07:27 +0600

    Update Fidar.js

--- scripts/commands/Fidar.js
+++ scripts/commands/Fidar.js
@@ -1,6 +1,6 @@
  const fs = require("fs");
 module.exports.config = {
-    name: "Fidar Voice",
+    name: "Fidar Emoji",
     version: "1.0.0",
     permssion: 0,
     credits: "Rasha",
@@ -13,15 +13,15 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("🍼")==0 || event.body.indexOf("🍼🍼")==0 || event.body.indexOf("ফিডার")==0 {
+	if (event.body.indexOf("🍼")==0 || event.body.indexOf("🍼🍼")==0 || event.body.indexOf("ফিডার")==0 || event.body.indexOf("👶")==0) {
 		var msg = {
-				body: "এই নেও বাবু ফিডার খাও, আমাকে তো আর খাইতে পারবা না🤭",
+				body: "এই নেও বাবু ফিডার খাও, আমাকে তো আর খাইতে পারবা না🤭🤭",
 				attachment: fs.createReadStream(__dirname + `/Nayan/Fidar.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🍼", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😍", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+	}
